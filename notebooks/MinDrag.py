d9e1b17bda6b34c44257072a5a3e4dd0b73082ca
federicoangioni <f.angioni@student.tudelft.nl>
2025-09-20 13:01:45 +0200

    Add MinDrag homepage (not yet completed)

---
+++ notebooks/MinDrag.py
@@ -0,0 +1,41 @@
+import marimo
+
+__generated_with = "0.15.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    # Initialization code that runs before all other cells
+    import marimo as mo
+
+    # Import dependencies
+    from core import _defaults
+    from plotly.subplots import make_subplots
+    import plotly.graph_objects as go
+    import plotly.express as px
+    import numpy as np
+    from core import atmos
+    from core import aircraft as ac
+    from core.aircraft import velocity, horizontal_constraint, power, drag
+
+    # Set local/online filepath
+    _defaults.FILEURL = _defaults.get_url()
+
+    # Plotly dark mode template
+    _defaults.set_plotly_template()
+
+    # Data directory
+    data_dir = str(mo.notebook_location() / "public" / "AircraftDB_Standard.csv")
+    return
+
+
+@app.cell
+def _():
+    # Set navbar on the right
+    _defaults.set_sidebar()
+    return
+
+
+if __name__ == "__main__":
+    app.run()
